a5821261d51540b123339edc89e4ca7fe95e2860
Ravi Shankar Mehta <ravi.mehta@atpl.live>
2024-08-09 13:08:32 +0530

    intregrate contacup api and request a ourest api

--- src/components/Request/Request.jsx
+++ src/components/Request/Request.jsx
@@ -8,18 +8,45 @@ import {
   VStack,
   Textarea,
 } from '@chakra-ui/react';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { courseRequest } from '../../redux/actions/other';
+import { useDispatch, useSelector } from 'react-redux';
+import { toast } from 'react-hot-toast';
 
 const Request = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [course, setCourse] = useState('');
+
+  const dispatch = useDispatch();
+  const {
+    loading,
+    error,
+    message: stateMessage,
+  } = useSelector(state => state.admin);
+
+  const submitHandler = e => {
+    e.preventDefault();
+    dispatch(courseRequest(name, email, course));
+  };
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error);
+      dispatch({ type: 'clearError' });
+    }
+    if (stateMessage) {
+      toast.success(stateMessage);
+      dispatch({ type: 'clearMessage' });
+    }
+  }, [dispatch, error, stateMessage]);
+
   return (
     <Container h={'92vh'}>
       <VStack h={'full'} justifyContent={'center'} spacing={16}>
         <Heading children="Request New Course" />
-        <form style={{ width: '100%' }}>
+        <form onSubmit={submitHandler} style={{ width: '100%' }}>
           <Box my={'4'}>
             <FormLabel htmlFor="name" children="Name" />
             <Input
@@ -32,7 +59,7 @@ const Request = () => {
               focusBorderColor="red.500"
             />
           </Box>
-      <Box my={'4'}>
+          <Box my={'4'}>
             <FormLabel htmlFor="email" children="Email Address" />
             <Input
               required
@@ -44,7 +71,7 @@ const Request = () => {
               focusBorderColor="red.500"
             />
           </Box>
-      <Box my={'4'}>
+          <Box my={'4'}>
             <FormLabel htmlFor="course" children="Course" />
             <Textarea
               required
@@ -55,17 +82,18 @@ const Request = () => {
               focusBorderColor="red.500"
             />
           </Box>
-    
+
           <Box>
-            <Button my={4} colorScheme="red" type="submit">
-            Send Mail
+            <Button isLoading={loading} my={4} colorScheme="red" type="submit">
+              Send Mail
             </Button>
             <Box my={'4'}>
               See Available Course ! ?{'  '}
               <Link to={'/courses'}>
                 <Button colorScheme="red" variant={'link'}>
                   Click
-                </Button> {"  "}
+                </Button>{' '}
+                {'  '}
                 here
               </Link>
             </Box>
@@ -76,5 +104,4 @@ const Request = () => {
   );
 };
 
-
-export default Request
+export default Request;
